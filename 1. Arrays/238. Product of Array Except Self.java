b5f557f6fa9e5310a779b0e0e6d0e63b694d727d
MD NADEEM ASGAR <nadeemasgar18@gmail.com>
2021-04-29 00:09:06 +0530

    More Codes

--- 1. Arrays/238. Product of Array Except Self.java
+++ 1. Arrays/238. Product of Array Except Self.java
@@ -33,3 +33,33 @@ class Solution {
         return ans;
     }
 }
+
+// 2nd Approach
+// Follow up:
+// Could you solve it in O(n) time complexity and without using division?
+// Could you solve it with O(1) constant space complexity? (The output array does not count as extra space for space complexity analysis.)
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int[] ans = new int[nums.length];
+        int product = 1;
+        
+        // Prefix Product
+        for(int i = 0; i < nums.length; i++) {
+            product *= nums[i];
+            ans[i] = product;
+        }
+        
+        int suffix = 1;
+        
+        for(int i = nums.length - 1; i >= 0; i--) {
+            
+            int temp = i > 0 ? ans[i-1] : 1;
+            ans[i] = temp * suffix;
+            suffix *= nums[i];
+        }
+        
+        return ans;
+    }
+}
